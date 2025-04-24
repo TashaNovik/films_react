9be9e75bbf441a98af7d868502c4549fc28a73ee
Natalia Novikova <novikova.nm@phystech.edu>
2025-04-24 22:32:27 +0300

    Чекбоксы смещены вправо

--- app/src/components/FilterBar.jsx
+++ app/src/components/FilterBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // Добавляем Heading в импорты
-import {Box, Text, Flex, Heading} from '@chakra-ui/react';
+import {Box, Text, Flex, Heading, Spacer, HStack} from '@chakra-ui/react';
 import {Checkbox} from "@chakra-ui/react";
 import {HiCheckCircle} from "react-icons/hi";
 import CircleCheckbox from "./CircleCheckbox.jsx";
@@ -9,19 +9,20 @@ function FilterBar() {
     return (
         <Flex
             alignItems="center"
-            justifyContent="space-between"
-            mb={6}
-            flexWrap="wrap"
-            gap={4}
+            width="100%" p={4}
+
 
         >
             <Heading as="h2" size="xl" fontWeight="bold">
                 Фильмы
             </Heading>
-            <CircleCheckbox label="Боевик" colorPalette="red" borderColor="red"></CircleCheckbox>
-            <CircleCheckbox label="Триллер" colorPalette="green" borderColor="green"></CircleCheckbox>
-            <CircleCheckbox label="Комедия" colorPalette="blue" borderColor="blue"></CircleCheckbox>
-            <CircleCheckbox label="Драма" colorPalette="black" borderColor="black"></CircleCheckbox>
+            <Spacer/>
+            <HStack spacing={4}>
+                <CircleCheckbox label="Боевик" colorPalette="red" borderColor="red"></CircleCheckbox>
+                <CircleCheckbox label="Триллер" colorPalette="green" borderColor="green"></CircleCheckbox>
+                <CircleCheckbox label="Комедия" colorPalette="blue" borderColor="blue"></CircleCheckbox>
+                <CircleCheckbox label="Драма" colorPalette="black" borderColor="black"></CircleCheckbox>
+            </HStack>
         </Flex>
     );
 }
